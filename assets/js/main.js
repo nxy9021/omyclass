858d8039eac6a1ea02a308123c35b8ec6c2e0a1f
Nanxi Yu <nanxiyu@gmail.com>
2021-03-22 02:29:48 -0500

    loaded charactors and background animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// the following 2 lines are for modulize later
 // import Preloader from './scenes/preloader.js';
 // import Lvl1 from './scenes/lvl1.js';
 
@@ -5,7 +6,103 @@ const config = {
   type: Phaser.CANVAS,
   width: 900,
   height: 640,
-//   scene: [Preloader, Lvl1],
+  scene: {
+    preload: preload,
+    create: create,
+    update: update,
+  },
+
+  // the following 2 lines are for modulize later
+  // scene: [Preloader, Lvl1],
+  // load background
 };
+
+function preload() {
+  // background reference
+  this.load.image('ref', 'assets/img/lvl1layout.png');
+
+  // solid background
+  this.load.image('bgBeige', 'assets/img/solidbg/bg_beige.png');
+  this.load.image('bgBlue', 'assets/img/solidbg/bg_blue.png');
+  this.load.image('bgGreen', 'assets/img/solidbg/bg_green.png');
+  this.load.image('bgRed', 'assets/img/solidbg/bg_red.png');
+  this.load.image('bgYellow', 'assets/img/solidbg/bg_yellow.png');
+
+
+  // animation background
+  this.load.animation('booksData', 'assets/img/books/books.json');
+
+  this.load.atlas(
+    'books',
+    'assets/img/books/books.png',
+    'assets/img/books/books.json'
+  );
+
+  // charactors
+  this.load.image('boy1', 'assets/img/charactors/boy1.png');
+  this.load.image('boy2', 'assets/img/charactors/boy2.png');
+  this.load.image('girl1', 'assets/img/charactors/girl1.png');
+  this.load.image('girl2', 'assets/img/charactors/girl2.png');
+  this.load.image('girl3', 'assets/img/charactors/girl3.png');
+  this.load.image('girl4', 'assets/img/charactors/girl4.png');
+
+  //   this.load.spritesheet('dude', 'assets/img/dude.png', {
+  //     frameWidth: 32,
+  //     frameHeight: 48,
+  //   });
+}
+
+function create() {
+  // background reference
+  this.add.image(460, 320, 'ref').setScale(0.6, 0.6);
+
+  // default yellow background
+
+  // book background for animation
+  this.books = this.add.sprite(400, 200, 'books').setScale(0.95, 0.95);
+
+  // sort framenames in order
+  let framenames = this.textures.get('books').getFrameNames().sort();
+
+  // see if frames are sorted in order
+  // console.log(framenames);
+
+  // For each framename, map it into animationFrame
+  // phaser 3 example wants to repeatly write the following 4 lines for each animation but this is lazier
+  // {
+  //  "key": "books",
+  //  "frame": "01_books",
+  //  "duration": 0,
+  //  "visible": false
+  // }
+
+  let animationFrames = framenames.map((framename) => {
+    return {
+      key: 'books',
+      frame: framename,
+    };
+  });
+
+  this.anims.create({
+    key: 'booksAnimation',
+    frames: animationFrames,
+    frameRate: 24,
+    repeat: -1,
+  });
+
+  this.books.play('booksAnimation');
+  // display the array of each animationFrame
+  // console.log(animationFrames);
+
+  // charactors
+  this.add.image(155, 200, 'boy1').setScale(0.6, 0.6);
+  this.add.image(397, 200, 'boy2').setScale(0.6, 0.6);
+  this.add.image(642, 200, 'girl1').setScale(0.6, 0.6);
+  this.add.image(155, 413, 'girl2').setScale(0.6, 0.6);
+  this.add.image(397, 413, 'girl3').setScale(0.6, 0.6);
+  this.add.image(642, 413, 'girl4').setScale(0.6, 0.6);
+}
+
+function update() {}
 
 const game = new Phaser.Game(config);
